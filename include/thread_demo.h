0aa7cdfbbf572c9fdbe55f52b457059da696e343
Carlo <gcrisis1@gmail.com>
2019-06-11 16:31:54 +0800

    thread demo

--- include/thread_demo.h
+++ include/thread_demo.h
@@ -5,4 +5,9 @@
 extern char * thread_str[];
 extern DEMO_SHOW thread_demo_list[];
 
+void pthread_create_demo();
+void pthread_exit_demo();
+void pthread_cancel_demo();
+void pthread_mutex_demo();
+
 #endif
--- src/thread_demo.c
+++ src/thread_demo.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include "thread_demo.h"
+#include <pthread.h>
 
        
 char * thread_str[]=
 	{
 		"pthread_create",
-		"pthread_exit",
-		"pthread_join",
+		"pthread_exit/pthread_join",
 		"pthread_cancel",
 		"pthread_cleanup_push/pthread_cleanup_pop",
 		"pthread_detach",
@@ -18,4 +19,60 @@ char * thread_str[]=
 DEMO_SHOW thread_demo_list[]=
 	{
 		{NULL,NULL},
+		{pthread_create_demo,""},
+		{pthread_exit_demo,""},
+		{pthread_cancel_demo,""},
+		{NULL,""},
+		{NULL,""},
+		{pthread_mutex_demo,""},
 	};
+	
+void * thr1(void * a)
+{
+		pthread_t thr;
+	thr = pthread_self();
+	printf("pid = %d \nthis is child thread %lx\n",getpid(),thr);
+	
+	return ((void *)0);
+}
+void pthread_create_demo()
+{
+	pthread_t thr_child;
+	pthread_t thr_fath;
+	pthread_create(&thr_child,NULL,thr1,NULL);
+	thr_fath = pthread_self();
+	printf("pid = %d \nthis is fath thread %lx\n",getpid(),thr_fath);
+	sleep(1);
+}
+
+void * fn1(void *arg)
+{
+	printf("thread 1 return\n");
+	return((void *)1);
+}
+void * fn2(void *arg)
+{
+	printf("thread 2 exit\n");
+	pthread_exit((void *)2);
+}
+void pthread_exit_demo()
+{
+	pthread_t thr_child[2];
+	void * tret;
+	pthread_create(&thr_child[0],NULL,fn1,NULL);
+	pthread_create(&thr_child[1],NULL,fn2,NULL);
+	
+	pthread_join(thr_child[0],&tret); /*get thread return status*/
+	printf("thread 1 return %p\n",tret);
+	pthread_join(thr_child[1],&tret);
+	printf("thread 2 return %p\n",tret);
+}
+
+void pthread_cancel_demo()
+{
+	
+}
+void pthread_mutex_demo()
+{
+	
+}
